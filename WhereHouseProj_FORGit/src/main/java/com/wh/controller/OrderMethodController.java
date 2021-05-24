8736558b2d4c79905000cb0d319852acc5e9becb
Animesh <Animesh@DESKTOP-3CELJKL>
2021-05-24 22:16:49 +0530

    22

--- WhereHouseProj_FORGit/src/main/java/com/wh/controller/OrderMethodController.java
+++ WhereHouseProj_FORGit/src/main/java/com/wh/controller/OrderMethodController.java
@@ -2,6 +2,7 @@ package com.wh.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.wh.model.OrderMethod;
 import com.wh.service.IOrderMethodService;
 
@@ -39,5 +40,26 @@ public class OrderMethodController {
        m.addAttribute("orderMethods",orderMethods);
        return "showOrderMethod";
 	}//show
-	
+
+	@GetMapping("/delete")
+	public String deleteOrderMethod(@RequestParam Integer  orderMId,RedirectAttributes m) {
+		service.deleteByOrderMethodId(orderMId);
+		m.addFlashAttribute("id1"," id "+ orderMId+" Is deleted");
+		return "redirect:/orderMethod/show";
+	}//delete
+	/*
+	 * for edit
+	 */
+	@GetMapping("/edit")
+	public String editOrderMethod(@RequestParam Integer orderMId, @ModelAttribute OrderMethod m) {
+		OrderMethod om=service.getOrderMethodModelById(orderMId);
+		BeanUtils.copyProperties(om, m);
+		return "OrderMethodForm";
+	}//delete
+	@PostMapping("/edit")
+	public String editSaveOrderMethod(@ModelAttribute OrderMethod m, RedirectAttributes m1) {
+		Integer id=service.saveOrderMethod(m);
+		m1.addFlashAttribute("id1"," OrderMethod with ID: " +id+" is added");
+		return "redirect:/orderMethod/show";
+	}//delete
 }//class
